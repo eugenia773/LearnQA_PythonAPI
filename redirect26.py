fc11de410d1741f62de3cd3ea551a3987fb77ca6
Evgeniya Skachkova <evgsk2@INT.KRONSHTADT.RU>
2022-07-23 15:40:30 +0300

    Correction for ex.6 lesson 2 added

--- redirect26.py
+++ redirect26.py
@@ -2,8 +2,6 @@ import requests
 
 response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
 
-resp = response.history
-elem = len(resp)
-
-print("Number of redirects =", elem - 1)
-print("Result URL:", resp[elem - 1].url)
+redirect_count = len(response.history)
+print("Number of redirects =", redirect_count)
+print("Result URL:", response.url)
