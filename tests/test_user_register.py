18654e87dc638402eb097162e7d85bb7100e7a52
Evgeniya Skachkova <evgsk2@INT.KRONSHTADT.RU>
2022-09-17 16:48:13 +0300

    Register modified with prepare_registration_data

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -28,28 +28,14 @@ class TestUserRegister(BaseCase):
         self.email = f"{base_part}{random_part}@{domain}"
 
     def test_create_user_successfully(self):
-        data = {
-            'username': 'learnqa',
-            'firstName': 'learnqa',
-            'lastName': 'learnqa',
-            'email': self.email,
-            'password': '123'
-        }
-
+        data = self.prepare_registration_data()
         response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
         Assertions.assert_code_status(response, 200)
         Assertions.assert_json_has_key(response, "id")
 
     def test_create_user_with_existing_email(self):
         email = 'vinkotov@example.com'
-        data = {
-            'username': 'learnqa',
-            'firstName': 'learnqa',
-            'lastName': 'learnqa',
-            'email': email,
-            'password': '123'
-        }
-
+        data = self.prepare_registration_data(email)
         response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
         Assertions.assert_code_status(response, 400)
         assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", \
